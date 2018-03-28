351c0a82825f6c933d566069bbbb34a891fcb753
cheewoei1997 <cheewoei1997@gmail.com>
2018-03-28 11:18:35 +0800

    cleaning up

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Choice
+from .models import Question, Choice, Chart
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Chart)
